137c644046115bc35bbb72b5c8b3c72ce8766d68
Raul <raulmaestre14@gmail.com>
2024-01-19 11:39:56 -0500

    4.1-4.2 Exercises

---
+++ controllers/blogs.js
@@ -0,0 +1,33 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    if (!body) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+
+    blog.save()
+        .then(savedblog => {
+        response.json(savedblog)
+        })
+        .catch(error => next(error))
+    })
+
+module.exports = blogsRouter
