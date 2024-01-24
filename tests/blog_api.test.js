a2fcc4f84424b2ae3598243af85749ff5f7cf2fc
Raul <raulmaestre14@gmail.com>
2024-01-24 18:48:56 -0500

    4.23 Exercise *Still have to improve in tests, the initial blogs cause they should be as if they're created by root user, I tried it and it works, but it seems to break some tests with error code 500*

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const { requestLogger } = require('../utils/middleware')
+const User = require('../models/user')
+
 const initialBlogs = [
     {
         title: 'Go To Statement Considered Harmful',
@@ -27,8 +29,23 @@ const initialBlogs = [
     },
 ]
 
+let token 
 beforeEach(async () => {
+
+    await User.deleteMany({})
+    
+    const passwordHash = await bcrypt.hash('sekret',10)
+    //const passwd = await helper.passwdGen('sekret')
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+
+    const userData = { username: 'root', password: 'sekret' }
+    const response = await api.post('/api/login').send(userData)
+    token = response.body.token
+
     await Blog.deleteMany({})
+    //Save blogs with the post method breaks some test, need to improve this.
     const blogObjects = initialBlogs.map(blog => new Blog(blog))
     const promiseArray = blogObjects.map(blogObject => blogObject.save())
     Promise.all(promiseArray)
@@ -38,17 +55,18 @@ describe('Getting blogs', () => {
     test('blogs are returned as json', async () => {
         await api
           .get('/api/blogs')
+          .set('Authorization', `Bearer ${token}`)
           .expect(200)
           .expect('Content-Type', /application\/json/)
       }, 100000)
       
     test('all blogs are returned', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         expect(response.body).toHaveLength(initialBlogs.length)
     }, 100000)
         
     test('a specific blog is within the returned blogs', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
     
         const titles = response.body.map(r => r.title)
     
@@ -58,17 +76,17 @@ describe('Getting blogs', () => {
     }, 100000)
       
     test('ID property', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
     
         const blogs = response.body
         const oneBlog = blogs[1]
-        console.log(oneBlog)
     
         expect(oneBlog.id).toBeDefined()
     }, 100000)
 })
 
 describe('Creating blogs', () => {
+
     test('Creating a new blog', async () => {
         const newBlog = {
             title: 'Go To Statement Considered Harmful',
@@ -77,10 +95,14 @@ describe('Creating blogs', () => {
             likes: 5
         }
         
-        const newBlogObject = new Blog(newBlog)
-        await newBlogObject.save()
+        await api
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
         
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         expect(response.body).toHaveLength(initialBlogs.length + 1)
     })
     
@@ -92,15 +114,14 @@ describe('Creating blogs', () => {
         }
     
         await api
-        .post('/api/blogs')
-        .set('Authorization', `Bearer ${user.token}`)
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
         
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         const addedBlog = response.body[3]
-        console.log(addedBlog)
         expect(addedBlog.id).toBeDefined()
     })
     
@@ -111,9 +132,10 @@ describe('Creating blogs', () => {
         }
     
         await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+            .expect(400)
         
         const newBlog2 = {
             author: 'Douglas Crockford',
@@ -121,28 +143,58 @@ describe('Creating blogs', () => {
         }
     
         await api
-        .post('/api/blogs')
-        .send(newBlog2)
-        .expect(400)
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog2)
+            .expect(400)
+    })
+
+    test('Unauthorized creating a new blog', async () => {
+        const newBlog = {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5
+        }
+        
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(401)
+            .expect('Content-Type', /application\/json/)
     })
 })
 
 describe('Deleting blogs', () => {
     test('deleting a blog', async () => {
-        const response = await api.get('/api/blogs')
+        const newBlog = {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5
+        }
+        
+        await api
+            .post('/api/blogs')
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+        
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         const blogs = response.body
-        const blog = blogs[1]
+        const blog = blogs[response.body.length - 1]
+
         const id = blog.id
     
         await api
             .delete(`/api/blogs/${id}`)
+            .set('Authorization', `Bearer ${token}`)
             .expect(204)
     })
 })
 
 describe('Updating blogs', () => {
     test('updating amount of likes', async () => {
-        const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         const blogs = response.body
         const blog = blogs[1]
         const id = blog.id
@@ -153,12 +205,12 @@ describe('Updating blogs', () => {
 
         await api
             .put(`/api/blogs/${id}`)
+            .set('Authorization', `Bearer ${token}`)
             .send(newInfo)
             .expect(200)
 
-        const newResponse = await api.get('/api/blogs')
+        const newResponse = await api.get('/api/blogs').set('Authorization', `Bearer ${token}`)
         const updatedBlog = newResponse.body[1]
-        console.log(updatedBlog)
         expect(updatedBlog.likes).toBe(20)
     })  
 
